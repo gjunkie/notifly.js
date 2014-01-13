949d58da2ec5d4f0ebbd24cf99e02e5236b39fe6
Freddie Carthy <fcarthy@gmail.com>
2014-01-13 15:52:48 -0800

    First commit

---
+++ notifly.js
@@ -0,0 +1,58 @@
+/**
+ * Notifly class
+ *
+ */
+
+(function(){
+
+  /**
+   * Class initiation.
+   *
+   * @param {String} string: The string that should be displayed as a notification.
+   * @param {Object} options: Object containing option overrides.
+   */
+  var Notifly = function (selector, options) {
+    var defaults = {
+      debug: false,
+      message: 'Generic user message',
+      sticky: false,
+      class: 'passive',
+      linger: 1000
+    };
+
+    var self = this;
+    this.options = $.extend({}, defaults, options || {});
+    this.el = $(selector);
+    this.el.addClass(this.options.class);
+    this.el.text(this.options.message);
+    this.el.on('click',function(){
+      self.close();
+    });
+
+    this.show();
+  };
+
+  /**
+   * Displays a notification.
+   */
+  Notifly.prototype.show = function () {
+    var self = this;
+    this.el.fadeIn(100);
+
+    if (!this.options.sticky) {
+      var timer = window.setTimeout(function(){
+            self.close();
+          }, this.options.linger);
+    }
+  };
+
+  /**
+   * Closes a notification.
+   */
+  Notifly.prototype.close = function () {
+    this.el.fadeOut(300);
+  };
+
+  window.rev3.Notifly = Notifly;
+
+}());
